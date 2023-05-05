92335aa234243a52ec00433cc6784c76e3c93740
EmilyWasu <emiliana.forestiere@studenti.unitn.it>
2023-05-05 11:44:18 +0200

    Update inverse

--- cpp/src/kinematics/inverse.cpp
+++ cpp/src/kinematics/inverse.cpp
@@ -25,46 +25,49 @@ T60 << re, pe;
         0, 0, 0, 1;
  
 auto T10f = [&](double th1) ->homoMatrix {
-    homoMatrix rix; rix << cos(th1), -sin(th1), 0, 0,
+    homoMatrix ret; ret << cos(th1), -sin(th1), 0, 0,
     sin(th1), cos(th1), 0, 0,
     0, 0, 1, D[0],
     0, 0, 0, 1;
-    return rix;
+    return ret;
 };
-auto T21f = [&](double th2) {
-    homoMatrix rix; rix <<cos(th2), -sin(th2), 0, 0,
+auto T21f = [&](double th2) ->homoMatrix{
+    homoMatrix ret; ret <<cos(th2), -sin(th2), 0, 0,
     0, 0, -1, 0,
     sin(th2), cos(th2), 0, 0,
     0, 0, 0, 1;
-    return rix;
+    return ret;
 };
 
-auto T32f = [&](double th3) {
-    homoMatrix rix; rix << cos(th3), -sin(th3), 0, A[1],
+auto T32f = [&](double th3) ->homoMatrix{
+    homoMatrix ret; ret << cos(th3), -sin(th3), 0, A[1],
     sin(th3), cos(th3), 0, 0,
     0, 0, 1, D[2],
     0, 0, 0, 1;
-    return rix;
+    return ret;
 };
 
-auto T43f = &(double th4) {
-    homoMatrix rix; rix <<cos(th4), -sin(th4), 0, A[2];
+auto T43f = [&](double th4) ->homoMatrix{
+    homoMatrix ret; ret <<cos(th4), -sin(th4), 0, A[2];
     sin(th4), cos(th4), 0, 0;
     0, 0, 1, D[3];
-    0, 0, 0, 1
+    0, 0, 0, 1;
+    return ret;
 };
 
-auto T54f = &(double th5) {
-    homoMatrix rix; rix <<cos(th5), -sin(th5), 0, 0;
-    0, 0, -1, -D(5);
+auto T54f = [&](double th5) ->homoMatrix{
+    homoMatrix ret; ret <<cos(th5), -sin(th5), 0, 0;
+    0, 0, -1, -D[4];
     sin(th5), cos(th5), 0, 0;
-    0, 0, 0, 1
+    0, 0, 0, 1;
+    return ret;
 };
-auto T65f =  &(double th6) {
-    homoMatrix rix; rix <<cos(th6), -sin(th6), 0, 0;
+auto T65f =  [&](double th6) ->homoMatrix{
+    homoMatrix ret; ret <<cos(th6), -sin(th6), 0, 0;
     0, 0, 1, D[5];
     -sin(th6), -cos(th6), 0, 0;
-    0, 0, 0, 1
+    0, 0, 0, 1;
+    return ret;
 };
 
 
@@ -72,50 +75,53 @@ auto T65f =  &(double th6) {
  //Finding th1
  Eigen::Matrix<double, 4, 1>p50;
  Eigen::Matrix<double, 4, 1>mat;
- mat << 0, 0, -D[5], 1;
+ mat << 0.0, 0.0, -D[5], 1.0;
  p50 << T60 * mat;
- th1_1 = real(atan2(p50(2), p50(1)) + acos(D(4)/hypot(p50(2), p50(1))))+pi/2;
- th1_2 = real(atan2(p50(2), p50(1)) - acos(D(4)/hypot(p50(2), p50(1))))+pi/2;
+ th1_1 = real(atan2(p50(2), p50(1)) + acos(D[3]/hypot(p50(2), p50(1))))+pi/2;
+ th1_2 = real(atan2(p50(2), p50(1)) - acos(D[3]/hypot(p50(2), p50(1))))+pi/2;
  
  //finding th5
- double th5_1 = real(acos((p60(1)*sin(th1_1) - p60(2)*cos(th1_1)-D(4)) / D(6)));
- double th5_2 = -real(acos((p60(1)*sin(th1_1) - p60(2)*cos(th1_1)-D(4)) / D(6)));
- double th5_3 = real(acos((p60(1)*sin(th1_2) - p60(2)*cos(th1_2)-D(4)) / D(6)));
- double th5_4 = -real(acos((p60(1)*sin(th1_2) - p60(2)*cos(th1_2)-D(4)) / D(6)));
+ double th5_1 = real(acos((p60(1)*sin(th1_1) - p60(2)*cos(th1_1)-D[3]) / D[5]));
+ double th5_2 = -real(acos((p60(1)*sin(th1_1) - p60(2)*cos(th1_1)-D[3]) / D[5]));
+ double th5_3 = real(acos((p60(1)*sin(th1_2) - p60(2)*cos(th1_2)-D[3]) / D[5]));
+ double th5_4 = -real(acos((p60(1)*sin(th1_2) - p60(2)*cos(th1_2)-D[3]) / D[5]));
  
 
   
   //related to th11 a th51
-  T06 = inv(T60);
-  Xhat = T06(1:3,1);
-  Yhat = T06(1:3,2);
+  T06 = T60.inverse();
+  Xhat << T06(0,0), T06(1,0), T06(2,0);
+  //(1:3,1);
+  Yhat << T06(0,1), T06(1,1), T06(2,1);
+  //(1:3,2);
   
   th6_1 = real(atan2(((-Xhat(2)*sin(th1_1)+Yhat(2)*cos(th1_1)))/sin(th5_1), ((Xhat(1)*sin(th1_1)-Yhat(1)*cos(th1_1)))/sin(th5_1)));
-  %related to th11 a th52
+  //related to th11 a th52
   th6_2 = real(atan2(((-Xhat(2)*sin(th1_1)+Yhat(2)*cos(th1_1))/sin(th5_2)), ((Xhat(1)*sin(th1_1)-Yhat(1)*cos(th1_1))/sin(th5_2))));
-  %related to th12 a th53
+  //related to th12 a th53
   th6_3 = real(atan2(((-Xhat(2)*sin(th1_2)+Yhat(2)*cos(th1_2))/sin(th5_3)), ((Xhat(1)*sin(th1_2)-Yhat(1)*cos(th1_2))/sin(th5_3))));
-  %related to th12 a th54
+  //related to th12 a th54
   th6_4 = real(atan2(((-Xhat(2)*sin(th1_2)+Yhat(2)*cos(th1_2))/sin(th5_4)), ((Xhat(1)*sin(th1_2)-Yhat(1)*cos(th1_2))/sin(th5_4))));
   
  
  
 
  T41m = inv(T10f(th1_1))*T60*inv(T65f(th6_1))*inv(T54f(th5_1));
- p41_1 = T41m(1:3,4);
+ p41_1 << T41m(0,3), T41m(1,3), T41m(2,3);
+ //(1:3,4);
  p41xz_1 = hypot(p41_1(1), p41_1(3));
  
  
  T41m = inv(T10f(th1_1))*T60*inv(T65f(th6_2))*inv(T54f(th5_2));
- p41_2 = T41m(1:3,4);
+ p41_2 = T41m(0,3), T41m(1,3), T41m(2,3);  //T41m(1:3,4);
  p41xz_2 = hypot(p41_2(1), p41_2(3));
  
  T41m = inv(T10f(th1_2))*T60*inv(T65f(th6_3))*inv(T54f(th5_3));
- p41_3 = T41m(1:3,4);
+ p41_3 = T41m(0,3), T41m(1,3), T41m(2,3); //T41m(1:3,4);
  p41xz_3 = hypot(p41_3(1), p41_3(3));
  
  T41m = inv(T10f(th1_2))*T60*inv(T65f(th6_4))*inv(T54f(th5_4));
- p41_4 = T41m(1:3,4);
+ p41_4 = T41m(0,3), T41m(1,3), T41m(2,3); //T41m(1:3,4);
  double p41xz_4 = hypot(p41_4(1), p41_4(3));
  
  //Computation of the 8 possible values for th3    
@@ -141,36 +147,36 @@ auto T65f =  &(double th6) {
  double th2_8 = real(atan2(-p41_4(3), -p41_4(1))-asin((A[2]*sin(th3_4))/p41xz_4));
  
  //find th4
- homoMatrix T43m = inv(T32f(th3_1))*inv(T21f(th2_1))*inv(T10f(th1_1))*T60*inv(T65f(th6_1))*inv(T54f(th5_1));
+ homoMatrix T43m = T32f(th3_1).inverse() * T21f(th2_1).inverse() * T10f(th1_1).inverse() * T60 * T65f(th6_1).inverse() * T54f(th5_1).inverse();
  coordinates Xhat43;
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_1 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_2))*inv(T21f(th2_2))*inv(T10f(th1_1))*T60*inv(T65f(th6_2))*inv(T54f(th5_2));
+ T43m = T32f(th3_2).inverse() * T21f(th2_2).inverse() * T10f(th1_1).inverse() * T60 * T65f(th6_2).inverse() * T54f(th5_2).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_2 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_3))*inv(T21f(th2_3))*inv(T10f(th1_2))*T60*inv(T65f(th6_3))*inv(T54f(th5_3));
+ T43m = T32f(th3_3).inverse() * T21f(th2_3).inverse() * T10f(th1_2).inverse() * T60 * T65f(th6_3).inverse() * T54f(th5_3).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_3 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_4))*inv(T21f(th2_4))*inv(T10f(th1_2))*T60*inv(T65f(th6_4))*inv(T54f(th5_4));
+ T43m = T32f(th3_4).inverse() * T21f(th2_4).inverse() * T10f(th1_2).inverse() * T60 * T65f(th6_4).inverse() * T54f(th5_4).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_4 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_5))*inv(T21f(th2_5))*inv(T10f(th1_1))*T60*inv(T65f(th6_1))*inv(T54f(th5_1));
+ T43m = T32f(th3_5).inverse() * T21f(th2_5).inverse() * T10f(th1_1).inverse() * T60 * T65f(th6_1).inverse() * T54f(th5_1).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_5 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_6))*inv(T21f(th2_6))*inv(T10f(th1_1))*T60*inv(T65f(th6_2))*inv(T54f(th5_2));
+ T43m = T32f(th3_6).inverse() * T21f(th2_6).inverse() * T10f(th1_1).inverse() * T60 * T65f(th6_2).inverse() * T54f(th5_2).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_6 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_7))*inv(T21f(th2_7))*inv(T10f(th1_2))*T60*inv(T65f(th6_3))*inv(T54f(th5_3));
+ T43m = T32f(th3_7).inverse() * T21f(th2_7).inverse() * T10f(th1_2).inverse() * T60 * T65f(th6_3).inverse() * T54f(th5_3).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_7 = real(atan2(Xhat43[1], Xhat43[0]));
  
- T43m = inv(T32f(th3_8))*inv(T21f(th2_8))*inv(T10f(th1_2))*T60*inv(T65f(th6_4))*inv(T54f(th5_4));
+ T43m = T32f(th3_8).inverse() * T21f(th2_8).inverse() * T10f(th1_2).inverse() * T60 * T65f(th6_4).inverse() * T54f(th5_4).inverse();
  Xhat43 << T43m(0,0), T43m(0,1), T43m(0,2); //T43m(1:3,1);
  double th4_8 = real(atan2(Xhat43[1], Xhat43[0]));
  
