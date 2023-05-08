f6edf8e1f15f08128166ac573822c5f27db92566
Emanuele Maccacaro <maccacaroemanuele@gmail.com>
2023-05-08 15:30:29 +0200

    correct some errors

--- cpp/src/kinematics/eigenMatrices.h
+++
@@ -1,6 +0,0 @@
-#include <eigen3/Eigen/Dense>
-
-typedef Eigen::Matrix<double, 6, 1> jointValues;
-typedef Eigen::Matrix<double, 4, 4> homoMatrix;
-typedef Eigen::Matrix<double, 3, 1> coordinates; //x,y,z
-typedef Eigen::Matrix<double, 3, 3> rotMatrix;
