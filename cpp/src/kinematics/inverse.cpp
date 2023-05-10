40551bd753d4a500f772df86eefd256a8de684a2
Emanuele Maccacaro <maccacaroemanuele@gmail.com>
2023-05-10 13:03:10 +0200

    modify inverse.cpp

--- cpp/src/kinematics/inverse.cpp
+++ cpp/src/kinematics/inverse.cpp
@@ -1,6 +1,6 @@
 #include <eigenMatrices.h>
 #include <kinematics.h>
-#include <complex.h>
+#include <complex.h> 
 #include <cmath>
 #include <iostream>
 
@@ -9,7 +9,7 @@ using namespace std;
 
 //Inverse Kineamtics of UR5
 
-jointValues inverse(coordinates pe, rotMatrix re) {
+jointValues inverse(coordinates& pe, rotMatrix& re) {
 
 jointValues th;
 
@@ -20,9 +20,9 @@ const double A[] = {0, -0.425, -0.3922, 0, 0, 0};
 const double D[] = {0.1625, 0, 0, 0.1333, 0.0997, 0.0996};
 
 homoMatrix T60;
-T60 << re[0], re[3], re[6], pe[0];
-        re[1],  re[4], re[7], pe[1];
-        re[2], re[5], re[8], pe[3];
+T60 << re[0], re[3], re[6], pe[0],
+        re[1],  re[4], re[7], pe[1],
+        re[2], re[5], re[8], pe[3],
         0, 0, 0, 1;
  
 //funzioni
@@ -51,25 +51,25 @@ auto T32f = [&](double th3) ->homoMatrix{
 };
 
 auto T43f = [&](double th4) ->homoMatrix{
-    homoMatrix ret; ret <<cos(th4), -sin(th4), 0, A[2];
-    sin(th4), cos(th4), 0, 0;
-    0, 0, 1, D[3];
+    homoMatrix ret; ret <<cos(th4), -sin(th4), 0, A[2],
+    sin(th4), cos(th4), 0, 0,
+    0, 0, 1, D[3],
     0, 0, 0, 1;
     return ret;
 };
 
 auto T54f = [&](double th5) ->homoMatrix{
-    homoMatrix ret; ret <<cos(th5), -sin(th5), 0, 0;
-    0, 0, -1, -D[4];
-    sin(th5), cos(th5), 0, 0;
+    homoMatrix ret; ret <<cos(th5), -sin(th5), 0, 0,
+    0, 0, -1, -D[4],
+    sin(th5), cos(th5), 0, 0,
     0, 0, 0, 1;
     return ret;
 };
 
 auto T65f =  [&](double th6) ->homoMatrix{
-    homoMatrix ret; ret <<cos(th6), -sin(th6), 0, 0;
-    0, 0, 1, D[5];
-    -sin(th6), -cos(th6), 0, 0;
+    homoMatrix ret; ret <<cos(th6), -sin(th6), 0, 0,
+    0, 0, 1, D[5],
+    -sin(th6), -cos(th6), 0, 0,
     0, 0, 0, 1;
     return ret;
 };
@@ -81,8 +81,8 @@ auto T65f =  [&](double th6) ->homoMatrix{
  Eigen::Matrix<double, 4, 1>mat;
  mat << 0.0, 0.0, -D[5], 1.0;
  p50 << T60 * mat;
- th(0,0) = real(atan2(p50[1], p50[0])) * complex_converter + acos(D[3] / (sqrt(pow(p50[1], 2) + pow(p50[0], 2)))) + pi/2;
- th(0,1) = real(atan2(p50[1], p50[0])) * complex_converter + acos(D[3] / (sqrt(pow(p50[1], 2) + pow(p50[0], 2)))) + pi/2;
+ th(0,0) = real(atan2(p50[1], p50[0])) * complex_converter + acos(D[3] / (sqrt(pow(p50[1], 2) + pow(p50[0], 2)))) + M_PI/2;
+ th(0,1) = real(atan2(p50[1], p50[0])) * complex_converter + acos(D[3] / (sqrt(pow(p50[1], 2) + pow(p50[0], 2)))) + M_PI/2;
  
  //finding th5
  th(4,0) = real(acos((pe[0] * sin(th(0,0)) - pe[1] * cos(th(0,0)) - D[3]) / D[5] * complex_converter));
@@ -93,7 +93,7 @@ auto T65f =  [&](double th6) ->homoMatrix{
 
   
 //related to th11 a th51
-T06 = T60.inverse();
+homoMatrix T06 = T60.inverse();
 coordinates Xhat, Yhat;
 Xhat << T06(0,0), T06(1,0), T06(2,0);
 Yhat << T06(0,1), T06(1,1), T06(2,1);
@@ -115,15 +115,18 @@ p41_1 << T41m(0,3), T41m(1,3), T41m(2,3);
 double p41xz_1 = sqrt(pow(p41_1[0], 2) + pow(p41_1[2], 2));
  
 T41m = T10f(th(0,0)).inverse() * T60 * T65f(th(5,1)).inverse() * T54f(th(4,1)).inverse();
-p41_2 = T41m(0,3), T41m(1,3), T41m(2,3);
-p41xz_2 = sqrt(pow(p41_2[0], 2) + pow(p41_2[2]));
+coordinates p41_2;
+p41_2 << T41m(0,3), T41m(1,3), T41m(2,3);
+double p41xz_2 = sqrt(pow(p41_2[0], 2) + pow(p41_2[2]));
  
 T41m = T10f(th(0,1)).inverse() * T60 * T65f(th(5,2)).inverse() * T54f(th(4,2)).inverse();
-p41_3 = T41m(0,3), T41m(1,3), T41m(2,3);
-p41xz_3 = sqrt(pow(p41_3[0], 2) + pow(p41_3[2], 2));
+coordinates p41_3;
+p41_3 << T41m(0,3), T41m(1,3), T41m(2,3);
+double p41xz_3 = sqrt(pow(p41_3[0], 2) + pow(p41_3[2], 2));
  
 T41m = T10f(th(0,1)).inverse() * T60 * T65f(th(5,3)).inverse() * T54f(th(4,3)).inverse();
-p41_4 = T41m(0,3), T41m(1,3), T41m(2,3);
+coordinates p41_4;
+p41_4 << T41m(0,3), T41m(1,3), T41m(2,3);
 double p41xz_4 = sqrt(pow(p41_4[0], 2) + pow(p41_4[2], 2));
  
 //Computation of the 8 possible values for th3    
@@ -143,10 +146,10 @@ th(1,1) = real(atan2(-p41_2[2], -p41_2[0]) - asin((-A[2] * sin(th(2,1))) / p41xz
 th(1,2) = real(atan2(-p41_3[2], -p41_3[0]) - asin((-A[2] * sin(th(2,2))) / p41xz_3));
 th(1,3) = real(atan2(-p41_4[2], -p41_4[0]) - asin((-A[2] * sin(th(2,3))) / p41xz_4));
  
-th(1,4) = real(atan2(-p41_1[2], -p41_1[0]) - asin((A[2] * sin(th3_1)) / p41xz_1));
-th(1,5) = real(atan2(-p41_2[2], -p41_2[0]) - asin((A[2] * sin(th3_2)) / p41xz_2));
-th(1,6) = real(atan2(-p41_3[2], -p41_3[0]) - asin((A[2] * sin(th3_3)) / p41xz_3));
-th(1,7) = real(atan2(-p41_4[2], -p41_4[0]) - asin((A[2] * sin(th3_4)) / p41xz_4));
+th(1,4) = real(atan2(-p41_1[2], -p41_1[0]) - asin((A[2] * sin(th(2,0))) / p41xz_1));
+th(1,5) = real(atan2(-p41_2[2], -p41_2[0]) - asin((A[2] * sin(th(2,1))) / p41xz_2));
+th(1,6) = real(atan2(-p41_3[2], -p41_3[0]) - asin((A[2] * sin(th(2,2))) / p41xz_3));
+th(1,7) = real(atan2(-p41_4[2], -p41_4[0]) - asin((A[2] * sin(th(2,3))) / p41xz_4));
  
  //find th4
  homoMatrix T43m = T32f(th(2,0)).inverse() * T21f(th(1,0)).inverse() * T10f(th(0,0)).inverse() * T60 * T65f(th(5,0)).inverse() * T54f(th(4,0)).inverse();
@@ -194,4 +197,20 @@ th(3,7) = real(atan2(Xhat43[1], Xhat43[0]) * complex_converter);
 
 return th;
     
+}
+
+
+int main(){
+    coordinates pe;
+    pe << 0.4, 0.5, 0.6;
+
+    rotMatrix re;
+    re << 1, 0, 0,
+        0, 1, 0,
+        0, 0, 1;
+
+    jointValues inv = ur5Inverse(pe, re);
+    cout << "Joint angles: \n" << inv << endl;
+
+    return 0;
 }
