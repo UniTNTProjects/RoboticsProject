42fedf19b4128a86360415a47bd0c813e19bf01c
Emanuele Ricca <emanuele.ricca.10@gmail.com>
2023-05-26 12:22:52 +0200

    fixes and formatting, modified FSM and demo pick up and place down

--- cpp/include/controller.h
+++ cpp/include/controller.h
@@ -26,19 +26,35 @@ private:
     ros::Publisher pub_gripper_diameter;
     ros::Subscriber sub_joint_state;
 
+    double acceptable_error = 0.05;
+
+    double max_x = 1;
+    double max_y = 1;
+    double max_z = 0.8;
+
+    double min_x = -1;
+    double min_y = -1;
+    double min_z = -1;
+
+    jointValues current_joints;
+    GripperStateVector current_gripper;
+    bool joint_initialized = false;
+
     void joint_state_callback(const sensor_msgs::JointState::ConstPtr &msg);
     void send_state(const jointValues &joint_pos);
     void sent_gripper_diameter(const int diameter);
     bool check_trajectory(vector<double *> traj, int step);
     jointValues second_order_filter(const jointValues &input, const double rate, const double settling_time);
     void init_filter(void);
-    double compute_error(const jointValues &first_vector, const jointValues &second_vector);
-    int *sort_ik_result(const Eigen::Matrix<double, 8, 6> &ik_result, const jointValues &initial_joints);
+    double calculate_distance(const jointValues &first_vector, const jointValues &second_vector);
+    int *sort_inverse(Eigen::Matrix<double, 8, 6> &inverse_kinematics_res, const jointValues &initial_joints);
+    double norm_angle(double angle);
 
 public:
     Controller(double loop_frequency);
     jointValues get_joint_state();
     GripperStateVector get_gripper_state();
+    pair<coordinates, rotMatrix> get_position();
     bool move_to(const coordinates &position, const rotMatrix &rotation, int steps);
     void move_gripper_to(const int diameter);
     void print_current_pos_rot();
--- cpp/src/FSM.cpp
+++ cpp/src/FSM.cpp
@@ -1,10 +1,15 @@
 #include "FSM.h"
 #include "ConcreteFSMState.h"
 
+using namespace std;
+
 FSM::FSM()
 {
     currentState = &Wait::getInstance();
-    controller = Controller(1000.);
+    controller = new Controller(1000.);
+    positions = new queue<pair<coordinates, rotMatrix>>();
+    this->moveGripperTo(60);
+    cout << "FSM initialized" << endl;
 }
 
 void FSM::setState(FSMState &newState)
@@ -15,6 +20,75 @@ void FSM::setState(FSMState &newState)
     currentState = &newState;
 
     currentState->enter(this);
+}
+
+pair<coordinates, rotMatrix> FSM::getNextPosition()
+{
+    pair<coordinates, rotMatrix> nextPos = positions->front();
+    positions->pop();
+
+    return nextPos;
+}
+
+void FSM::addPosition(coordinates pos, rotMatrix rot)
+{
+    cout << "Adding position to queue" << endl;
+
+    positions->push(make_pair(pos, rot));
+    cout << "Queue done" << endl;
+}
+
+bool FSM::isPositionQueueEmpty()
+{
+    return positions->empty();
+}
+
+bool FSM::moveTo(coordinates pos, rotMatrix rot)
+{
+    return controller->move_to(pos, rot, 20);
+}
+
+void FSM::moveGripperTo(int diameter)
+{
+    controller->move_gripper_to(diameter);
+}
+
+bool FSM::pickUp()
+{
+    pair<coordinates, rotMatrix> currentPos = controller->get_position();
+    currentPos.first(2) += 0.1;
+
+    if (moveTo(currentPos.first, currentPos.second))
+    {
+        moveGripperTo(20);
+        isGripping = true;
+        currentPos.first(2) -= 0.1;
+        if (moveTo(currentPos.first, currentPos.second))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool FSM::placeDown()
+{
+    pair<coordinates, rotMatrix> currentPos = controller->get_position();
+    currentPos.first(2) += 0.1;
+
+    if (moveTo(currentPos.first, currentPos.second))
+    {
+        moveGripperTo(60);
+        isGripping = false;
+        currentPos.first(2) -= 0.1;
+        if (moveTo(currentPos.first, currentPos.second))
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 void FSM::toggle()
@@ -29,8 +103,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "controller_ur5");
 
     FSM fsm;
-    fsm = FSM();
-    while (true)
+
+    while (!fsm.isDone)
     {
         fsm.toggle();
     }
